9e6060b209f1e6538ced18746c268ffcc39af612
LarissaSampaio <larisampaio@hotmail.com>
2021-10-14 19:43:13 -0300

    Insert atributes

---
+++ ConsoleClassePessoa/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClassePessoa
+{
+    class Pessoa
+    {
+        string nome;
+        string idade;
+        string rg;
+        string cpf;
+    }
+}
